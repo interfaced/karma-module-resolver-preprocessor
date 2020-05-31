bb8fade0a60579ae15c03596dbc2d3e49c2bc8f6
Brion Fuller <blfullerii@gmail.com>
2020-05-31 14:49:45 -0700

    Add all valid native esmodule imports     1) Moved to @babel/parser from espree (included handle @babel/types) to determine the type of node we are at     2) ecmaVersion option removed     3) resolveNodeModules option was added and defaults to false     4) Moved the addition of the extension below path rewrite (require.resolved requires no extension for module name import)

--- index.js
+++ index.js
@@ -1,107 +1,160 @@
-const espree = require('espree');
-const path = require('path');
+const path = require("path");
+const { parse } = require("@babel/parser");
+const { default: traverse } = require("@babel/traverse");
+const {
+  isImportDeclaration,
+  isStringLiteral,
+  isImport,
+  isExportNamedDeclaration,
+  isExportAllDeclaration,
+} = require("@babel/types");
 
 const defaultOptions = {
-	addExtension: 'js',
-	customResolver: null,
-	ecmaVersion: 6,
-	aliases: {}
+  addExtension: "js",
+  customResolver: null,
+  resolveNodeModules: false,
+  aliases: {},
 };
 
 const isLocalPath = (filename) =>
-	filename.startsWith('./') ||
-	filename.startsWith('../') ||
-	filename.startsWith('/');
+  filename.startsWith("./") ||
+  filename.startsWith("../") ||
+  filename.startsWith("/");
 
-const hasExtension = (filename, extension) => filename.endsWith('.' + extension);
+const hasExtension = (filename, extension) =>
+  filename.endsWith("." + extension);
 
 // This is copypasted from karma middleware
 const filePathToUrlPath = (filePath, basePath, urlRoot, proxyPath) => {
-	if (filePath.startsWith(basePath)) {
-		return proxyPath + urlRoot.substr(1) + 'base' + filePath.substr(basePath.length)
-	}
-	return proxyPath + urlRoot.substr(1) + 'absolute' + filePath
+  if (filePath.startsWith(basePath)) {
+    return (
+      proxyPath + urlRoot.substr(1) + "base" + filePath.substr(basePath.length)
+    );
+  }
+  return proxyPath + urlRoot.substr(1) + "absolute" + filePath;
 };
 
-const createModuleResolverPreprocessor = (karmaConfig, args = {}, config = {}, logger, helper) => {
-	const log = logger.create('preprocessor:module-resolver');
-	const options = helper.merge({}, defaultOptions, args, config);
+const createModuleResolverPreprocessor = (
+  karmaConfig,
+  args = {},
+  config = {},
+  logger,
+  helper
+) => {
+  const log = logger.create("preprocessor:module-resolver");
+  const options = helper.merge({}, defaultOptions, args, config);
 
-	// Process longer aliases first to avoid collision with shorter ones
-	const sortedAliases = Object.keys(options.aliases).sort().reverse();
+  // Process longer aliases first to avoid collision with shorter ones
+  const sortedAliases = Object.keys(options.aliases).sort().reverse();
 
-	// Normalize path to absolute
-	for (const alias of sortedAliases) {
-		options.aliases[alias] = path.resolve(karmaConfig.basePath, options.aliases[alias]);
-	}
+  // Normalize path to absolute
+  for (const alias of sortedAliases) {
+    options.aliases[alias] = path.resolve(
+      karmaConfig.basePath,
+      options.aliases[alias]
+    );
+  }
 
-	const resolvePath = (modulePath) => {
-		if (options.customResolver) {
-			return options.customResolver(modulePath);
-		}
+  const resolvePath = (modulePath) => {
+    if (options.customResolver) {
+      return options.customResolver(modulePath);
+    }
 
-		let result = modulePath;
+    let result = modulePath;
 
-		if (options.addExtension && !hasExtension(modulePath, options.addExtension)) {
-			result += '.' + options.addExtension;
-		}
+    if (!isLocalPath(result)) {
+      if (options.resolveNodeModules) {
+        const absolutePath = require.resolve(result);
 
-		if (!isLocalPath(result)) {
-			for (const alias of sortedAliases) {
-				if (result.startsWith(alias)) {
-					const pathUnderAlias = path.relative(alias, result);
-					const absolutePath = path.resolve(options.aliases[alias], pathUnderAlias);
-					result = filePathToUrlPath(
-						absolutePath,
-						karmaConfig.basePath,
-						karmaConfig.urlRoot,
-						karmaConfig.upstreamProxy ? karmaConfig.upstreamProxy.path : '/'
-					)
-				}
-			}
-		}
-		return result;
-	};
+        result = filePathToUrlPath(
+          absolutePath,
+          karmaConfig.basePath,
+          karmaConfig.urlRoot,
+          karmaConfig.upstreamProxy ? karmaConfig.upstreamProxy.path : "/"
+        );
+      } else {
+        for (const alias of sortedAliases) {
+          if (result.startsWith(alias)) {
+            const pathUnderAlias = path.relative(alias, result);
+            const absolutePath = path.resolve(
+              options.aliases[alias],
+              pathUnderAlias
+            );
+            result = filePathToUrlPath(
+              absolutePath,
+              karmaConfig.basePath,
+              karmaConfig.urlRoot,
+              karmaConfig.upstreamProxy ? karmaConfig.upstreamProxy.path : "/"
+            );
+          }
+        }
+      }
+    }
 
-	return (content, file, done) => {
-		log.debug('Processing "%s".', file.originalPath);
+    if (
+      options.addExtension &&
+      !hasExtension(modulePath, options.addExtension)
+    ) {
+      result += "." + options.addExtension;
+    }
+    return result;
+  };
 
-		let ast = null;
-		try {
-			ast = espree.parse(content, {
-				ecmaVersion: options.ecmaVersion,
-				sourceType: 'module'
-			});
-		} catch (e) {
-			log.error('%s\n  at %s', e.message, file.originalPath);
-			done(e, null);
-			return;
-		}
+  return (content, file, done) => {
+    log.debug('Processing "%s".', file.originalPath);
 
-		let patchedContent = content;
-		for (const node of ast.body.reverse()) {
-			// Imports can only be at top level as per specification
-			if (node.type === 'ImportDeclaration' && node.source.type === 'Literal') {
-				const resolved = resolvePath(node.source.value)
-					.replace(/\\/g, '\\\\'); // Great job, Windows
-				if (resolved !== node.source.value) {
-					const replacement = node.source.raw.replace(node.source.value, resolved);
-					log.debug('Replacing import from "%s" with "%s"', node.source.value, resolved);
-					patchedContent =
-						patchedContent.slice(0, node.source.start) +
-						replacement +
-						patchedContent.slice(node.source.end);
-				}
-			}
-		}
+    try {
+      const ast = parse(content, {
+        sourceType: "module",
+        plugins: ["dynamicImport", "exportNamespaceFrom"],
+      });
 
-		done(patchedContent);
-	}
+      let patchedContent = [];
+      let pointer = 0;
+      traverse(ast, {
+        enter(path) {
+          const node = path.node;
+          if (
+            isStringLiteral(node) &&
+            (isImport(path.parent.callee) ||
+              isImportDeclaration(path.parent) ||
+              isExportNamedDeclaration(path.parent) ||
+              isExportAllDeclaration(path.parent))
+          ) {
+            const resolved = resolvePath(node.value).replace(/\\/g, "\\\\"); // Great job, Windows
+            if (resolved !== node.value) {
+              log.debug(
+                'Replacing import from "%s" with "%s"',
+                node.value,
+                resolved
+              );
+              patchedContent.push(
+                content.slice(pointer, node.start + 1) + resolved
+              );
+              pointer = node.end - 1;
+            }
+          }
+        },
+      });
+      patchedContent.push(content.slice(pointer, content.length));
+      done(patchedContent.join(""));
+    } catch (e) {
+      log.error("%s\n  at %s", e.message, file.originalPath);
+      done(e, null);
+      return;
+    }
+  };
 };
 
-createModuleResolverPreprocessor.$inject = ['config', 'args', 'config.moduleResolverPreprocessor', 'logger', 'helper'];
+createModuleResolverPreprocessor.$inject = [
+  "config",
+  "args",
+  "config.moduleResolverPreprocessor",
+  "logger",
+  "helper",
+];
 
 // PUBLISH DI MODULE
 module.exports = {
-	'preprocessor:module-resolver': ['factory', createModuleResolverPreprocessor]
+  "preprocessor:module-resolver": ["factory", createModuleResolverPreprocessor],
 };
